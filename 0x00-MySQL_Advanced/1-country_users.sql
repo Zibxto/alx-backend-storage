edfc1edc27f1e8be69f991aaf2bb9a7b5f83bd48
zibxto <christianaziba@gmail.com>
2024-03-25 14:10:32 +0100

    solve task 2

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,6 +4,5 @@ CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
-    country ENUM('US', 'CO', 'TN') DEFAULT 'US',
-    CONSTRAINT chk_country CHECK (country IN ('US', 'CO', 'TN'))
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US'
 );
