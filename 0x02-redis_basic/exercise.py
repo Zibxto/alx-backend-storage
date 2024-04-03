2b87a92aa73d33c70185522bb8f060c0622cc5c4
zibxto <christianaziba@gmail.com>
2024-04-03 11:29:59 +0100

    solve task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,3 +17,24 @@ class Cache:
         id = str(uuid.uuid1())
         self._redis.mset({"{}".format(id): data})
         return id
+
+    def get(self, key: str, fn=None):
+        """
+        Retrieve data from Redis and convert
+        it back to the desired format.
+        """
+        result = self._redis.get(key)
+        if result is None:
+            return None  # Key does not exist, conserving Redis.get behavior
+        if fn is not None:
+            return fn(result)  # Convert data using the provided callable
+        else:
+            return result
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """Retrieve data from Redis and convert it to a string."""
+        return self.get(key, lambda data: data.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """Retrieve data from Redis and convert it to an integer."""
+        return self.get(key, lambda data: int(data))
